c98ff8e0e4ddf7cd7d1d589fa7d8a38246cee4a5
VariableReThrushRate <bgpilot16@gmail.com>
2024-03-15 19:19:13 -0700

    Removed junk

--- CMakeFiles/vitapong-velf.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/vitapong-velf"
-  "vitapong.velf"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/vitapong-velf.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
